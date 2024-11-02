814cce32605873f53a54b94f1436565acc94903e
Mohammed Shadin <pmshadin@localhost.localdomain>
2024-11-02 10:53:59 +0530

    Update main.py by adding dice command

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import interactions
 from interactions import*
 import os
 from dotenv import load_dotenv
+import random
 
 #Import token from .env file
 load_dotenv()
@@ -27,5 +28,26 @@ async def ping(ctx: SlashContext):
         color=interactions.Color.random()
         )
     await ctx.send(embeds=[pings])
+
+@slash_command(
+    name="dice",
+    description="Rolls a dice"
+)
+async def dice(ctx: SlashContext):
+    dice1 = ['⚀','⚁','⚂','⚃','⚄','⚅']
+    diceembed = Embed(
+        title="Dice Roll",
+        description="Rolling the dice...",
+        color=interactions.Color.random()
+    )
+    msg = await ctx.send(embeds=[diceembed])
+    await asyncio.sleep(5)
+
+    diceembed2 = Embed(
+        title="Dice Roll",
+        description=f"Rolled dice {random.choices(dice1)}",
+        color=interactions.Color.random()
+    )
+
     
 bot.start(token)
